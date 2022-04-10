8c919cbe9edf4e0271cc6154ca675676767fae60
md1789 <megan@eci-web.com>
2022-04-10 16:30:46 -0400

    Added event list template

---
+++ client/src/components/List/Event-List.js
@@ -0,0 +1,15 @@
+import React, { Fragment, useState } from 'react';
+
+// create a list item for each event in the events table with a name,
+// category, description, date, time, location, rating, and join button
+const EventList = () => {
+    return (
+        <Fragment>
+            <b-list-group>
+                <b-list-group-item></b-list-group-item>
+            </b-list-group>
+        </Fragment>
+    )
+}
+
+export default EventList;
