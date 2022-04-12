62ff16020c58b3b8653026e298c0738f944a0118
md1789 <megan@eci-web.com>
2022-04-12 11:36:56 -0400

    Added to backend

---
+++ server/controllers/registerController.js
@@ -0,0 +1,30 @@
+const usersDB = {
+    users: require('../users.json'),
+    setUsers: function (data) { this.users = data }
+}
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.bodt;
+    if (!user || !pwd ) return res.status(400).json({ 'message': 'Username and password are required' });
+    const duplicate = usersDB.users.find(person => person.username === user);
+    if (duplicate) return res.status(409);
+    try {
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+        const newUser = { "username": user, "password": hashedPwd };
+        usersDB.setUsers([...usersDB.users, newUser]);
+        await fsPromises.writeFile(
+            path.join(_dirname, '..', 'users.json'), 
+            JSON.stringify(usersDB.users)
+        );
+        console.log(usersDB.users);
+        res.status(201).json({ 'success': `New user ${user} created!` });
+    } catch (error) {
+        res.status(500).json({ 'message': error.message });
+    }
+}
+
+module.exports = { handleNewUser };
---
+++ server/server.js
@@ -0,0 +1,91 @@
+const pool = require("./db");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('.middleware/errorHandler');
+
+// middleware: functions that have access to the request object and response object and the next function in the request-response cycle
+app.use(logger);
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// routes
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+// post events
+app.post('/events', async(req, res) => {
+    try {
+       console.log(req.body); 
+       const newEvent =  await pool.query('INSERT INTO events (name, category, description, time, date, location, phone, email) VALUES($1, $2, $3, $4, $5, $6, $7, $8)', 
+       [name, category, description, time, date, location, phone, email]);
+       res.json(newEvent.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// get events
+app.get('/events', async(req, res) => {
+    try {
+       const allEvents = await pool.query('SELECT * FROM events');
+       res.json(allEvents.rows); 
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// get an event
+app.get('/events/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const event = await pool.query('SELECT * FROM events WHERE event_name = $1', [event_name]);
+        res.json(event.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// update an event
+app.put('/events/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, category, description, time, date, location, phone, email } = req.body;
+        const updateEvent = await pool.query('UPDATE events SET name = $1, category = $2, description = $3, time = $4, date = $5, location = $6, phone = $7, email = $8 WHERE event_name = $9', 
+        [name, category, description, time, date, location, phone, email, event_name]);
+        res.json('Events are updated')
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+//delete an event
+app.delete('/events/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteEvent = await pool.query('DELETE FROM events WHERE name = $1',
+        [event_name]);
+        res.json('Event was deleted');
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'html', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(5000, () => {
+    console.log('listening on port 5000');
+});
+
